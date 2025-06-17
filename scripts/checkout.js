849a63c413b760c8abae221f5e36c87d0d4dae2c
Kenneth Domdom <domdomkenneth23@gmail.com>
2025-06-17 23:35:58 +0800

    checkoutItem

---
+++ scripts/checkout.js
@@ -0,0 +1,11 @@
+import {cart,addToCart} from  '../data/cart.js';
+import { products} from '../data/products.js';
+
+
+let checkoutHTML=``;
+
+      cart.forEach((checkOutItem)=>{
+       
+      });
+
+        
