e6ab40bd1817ac7dde1489d738e5d5a925d019b9
Kenneth Domdom <domdomkenneth23@gmail.com>
2025-06-29 21:57:05 +0800

    debugging

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,3 +1,4 @@
 import { renderSummary } from "./checkout/RenderSummary.js";
-
+import { paymentSummary } from "./checkout/PaymentSummary.js";
 renderSummary();
+paymentSummary();
---
+++ scripts/checkout/PaymentSummary.js
@@ -0,0 +1,8 @@
+import { cart } from "../../data/cart.js";
+export function paymentSummary(){
+let total;
+ cart.forEach((cartItem)=>{
+   total += cartItem.
+ })
+}
+
