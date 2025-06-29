26eac6532fe76b8c2e0e353122fa8e22f13c2d7a
Kenneth Domdom <domdomkenneth23@gmail.com>
2025-06-29 23:17:57 +0800

    delivery option debugging

--- scripts/checkout/PaymentSummary.js
+++ scripts/checkout/PaymentSummary.js
@@ -1,13 +1,18 @@
 import { cart } from "../../data/cart.js";
 import { matchProduct } from "../../data/products.js";
+import DeliveryOptions from "../../data/DeliveryOptions.js";
 export function paymentSummary(){
 let total = 0;
-
+let shippingFee = 0;
 cart.forEach((cartItem)=>{
     const matchItem = matchProduct(cartItem.id);
     total += matchItem.priceCents * cartItem.quantity;
+    
 })
 
 console.log((total/100).toFixed(2));
+
+
+console.log(shippingFee)
 }
 
